629e9b80638a2358fdaa758c2adaddfbf109ddd8
Fábio Falcão <fabio.falcao@manta.io>
2025-09-23 11:27:16 +0100

    Update files

--- terraform/tm-poc/main.tf
+++ terraform/tm-poc/main.tf
@@ -20,7 +20,12 @@ data "aws_ami" "amazon_linux_2" {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
-
+  
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
